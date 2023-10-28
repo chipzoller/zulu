78529db4e648e76f6bb11c49abb8c3b15247097e
Chip Zoller <chipzoller@gmail.com>
2023-10-28 17:32:47 -0400

    try hugo
    Signed-off-by: Chip Zoller <chipzoller@gmail.com>

--- .devcontainer/tools.sh
+++ .devcontainer/tools.sh
@@ -2,3 +2,4 @@
 
 ## Install yq
 sudo wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/bin/yq && sudo chmod +x /usr/bin/yq
+wget https://github.com/gohugoio/hugo/releases/download/v0.119.0/hugo_0.119.0_linux-amd64.tar.gz -O hugo.tar.gz && tar -xvf hugo.tar.gz && sudo mv hugo /usr/bin/hugo
